2bfb672f7b1494d82004a7dbf6614ffa6fe2aadf
supersymmetry <maildumpert@gmail.com>
2013-02-17 17:18:02 +0100

    all scripts

---
+++ environment.sh
@@ -0,0 +1,18 @@
+export HEROKU_URL=""
+export HUBOT_ASANA_API_KEY="1aeQlQQj.0XWuAtMcuPbpNcl95DRSzyU"
+export HUBOT_MSTRANSLATE_APIKEY="DNYZfp/R/d1cdSgiGRkU3wmIozRSR/iS/8DpF9Jpzh4="
+export GRAPHITE_URL="metrics.solobit.net"
+export GRAPHITE_PORT="2004"
+export GRAPHITE_AUTH=""
+export HUBOT_GITHUB_TOKEN=""
+export HUBOT_GITHUB_USER="supersym"
+#HUBOT_GITHUB_API
+export HUBOT_DEPLOY_ROOM="33"
+export HUBOT_HARVEST_SUBDOMAIN="sbit.harvest.com"
+export HUBOT_XMPP_USERNAME="maildumpert@gmail.com"
+export HUBOT_XMPP_PASSWORD="masterM3"
+export HUBOT_XMPP_ROOMS="2"
+export HUBOT_IRC_NICK="k111807N"
+export HUBOT_IRC_ROOMS="#sotred"
+export HUBOT_IRC_SERVER="irc.freenode.net"
+
